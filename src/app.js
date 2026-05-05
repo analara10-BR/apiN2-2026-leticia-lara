c50dc088628cddefd28a833a2e48036e3c4518af
analara10-BR <ana.lara10@aluno.ifce.edu.br>
2026-05-05 00:27:10 -0300

    Update app.js
    Código app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+import express from "express";
+import { router } from "./routes/api.routes.js";
+import { debug } from "./middlewares/debug.middleware.js";
+import { notFound, errorHandler } from "./middlewares/errors.middleware.js";
+import { mcors } from "./middlewares/mcors.middleware.js";
 
+const app = express();
+
+app.use(mcors);
+app.use(express.json());
+app.use(debug);
+
+app.use("/api", router);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
