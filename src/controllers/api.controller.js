44c388b306f661583c73ae81300de9f1e3974a1f
analara10-BR <ana.lara10@aluno.ifce.edu.br>
2026-05-05 00:42:19 -0300

    Update api.controller.js
    Config api

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,7 +1,25 @@
-export const getInfo = (req, res) => {
-  res.json({
-    status: "ok",
-    mensagem: "API funcionando 🚀",
-    projeto: "API N2"
-  });
-};
+export default class ApiController {
+  constructor() {}
+
+  async getInfo(req, res) {
+    try {
+      res.json({ message: "Api Web em Node e Express." });
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao ao exibir dados da api!" });
+    }
+  }
+
+  async getInfoByParamId(req, res, next) {
+    try {
+      const id = Number(req.params.id);
+
+      if (id !== 42) {
+        throw new Error("Resposta Incorreta!");
+      }
+
+      res.json({ message: `ID: ${id}` });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
