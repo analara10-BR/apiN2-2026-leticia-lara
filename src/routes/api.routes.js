725a0002e472729296bce347c0eef5c8418cc34d
leticiaribeiro09 <leticia.ribeiro09@aluno.ifce.edu.br>
2026-05-07 10:12:21 -0300

    Update api.routes.js

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,15 +1,20 @@
 import { Router } from "express";
+
 import ApiController from "../controllers/api.controller.js";
+
 import verifyParamId from "../middlewares/verify.id.middleware.js";
 
 const router = Router();
 
 const controller = new ApiController();
 
-router.get("/info", (req, res) => controller.getInfo(req, res));
+router.get("/roupas",
+(req, res) => controller.getRoupas(req, res));
 
-router.get("/info/:id", verifyParamId, (req, res, next) =>
-  controller.getInfoByParamId(req, res, next),
+router.get("/roupas/:id",
+verifyParamId,
+(req, res, next) =>
+controller.getRoupaByParamId(req, res, next)
 );
 
-export { router };
+export default router;
