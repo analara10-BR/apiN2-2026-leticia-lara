ec7d7a0eec2c46db133b0c8473332a5c33a17a0c
Nayelle L. <nayelle.sousa10@aluno.ifce.edu.br>
2026-05-07 10:36:08 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,5 +3,9 @@ import app from "./app.js";
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+
+    console.log(
+        `Servidor do Brechó Online rodando em localhost:${PORT}`
+    );
+
 });
