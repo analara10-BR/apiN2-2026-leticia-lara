d58a618e7c9a4263e8e67ca5e796072f3f8d89d1
leticiaribeiro09 <leticia.ribeiro09@aluno.ifce.edu.br>
2026-05-07 10:14:39 -0300

    Update api.controller.js

--- src/controllers/api.controller.js
+++ src/controllers/api.controller.js
@@ -1,49 +1,103 @@
 export default class ApiController {
 
+    constructor() {}
+
+    roupas = [
+
+        {
+            id: 1,
+            nome: "Jaqueta Jeans",
+            tamanho: "M",
+            marca: "Levis",
+            preco: 80,
+            categoria: "Casaco",
+            disponivel: true
+        },
+
+        {
+            id: 2,
+            nome: "Vestido Vintage",
+            tamanho: "P",
+            marca: "Zara",
+            preco: 65,
+            categoria: "Vestido",
+            disponivel: true
+        },
+
+        {
+            id: 3,
+            nome: "Camisa Oversized",
+            tamanho: "G",
+            marca: "Nike",
+            preco: 90,
+            categoria: "Camisa",
+            disponivel: true
+        },
+
+        {
+            id: 4,
+            nome: "Calça Cargo",
+            tamanho: "42",
+            marca: "Adidas",
+            preco: 110,
+            categoria: "Calça",
+            disponivel: true
+        },
+
+        {
+            id: 5,
+            nome: "Moletom Preto",
+            tamanho: "M",
+            marca: "Puma",
+            preco: 95,
+            categoria: "Moletom",
+            disponivel: false
+        }
+
+    ];
+
+    // LISTAR ROUPAS
     async getRoupas(req, res) {
+
         try {
 
-            res.json([
-                {
-                    id: 1,
-                    nome: "Jaqueta Jeans",
-                    tamanho: "M",
-                    preco: 80
-                },
-
-                {
-                    id: 2,
-                    nome: "Vestido Vintage",
-                    tamanho: "P",
-                    preco: 65
-                }
-            ]);
+            res.json(this.roupas);
 
         } catch (error) {
 
             return res.status(500).json({
-                error: "Erro ao listar roupas"
+                error: "Erro ao exibir roupas!"
             });
 
         }
+
     }
 
+    // BUSCAR ROUPA POR ID
     async getRoupaByParamId(req, res, next) {
 
         try {
 
             const id = Number(req.params.id);
 
-            if (id !== 7) {
-                throw new Error("Roupa não encontrada");
+            const roupa = this.roupas.find(
+                roupa => roupa.id === id
+            );
+
+            if (!roupa) {
+
+                throw new Error("Roupa não encontrada!");
+
             }
 
-            res.json({
-                mensagem: `Roupa encontrada! ID: ${id}`
-            });
+            res.json(roupa);
 
         } catch (error) {
+
             next(error);
+
         }
+
     }
+
 }
