b745f4ad09832b482603535e35dac24c32fa6833
Nayelle L. <nayelle.sousa10@aluno.ifce.edu.br>
2026-05-07 10:31:22 -0300

    Update mcors.middleware.js

--- src/middlewares/mcors.middleware.js
+++ src/middlewares/mcors.middleware.js
@@ -1,9 +1,27 @@
 import cors from "cors";
 
 // Middleware para CORS
+
 export const mcors = cors({
-  origin: "http://localhost:5000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+
+    origin: "http://localhost:5000",
+
+    methods: [
+
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE"
+
+    ],
+
+    allowedHeaders: [
+
+        "Content-Type",
+        "Authorization"
+
+    ],
+
+    credentials: true,
+
 });
