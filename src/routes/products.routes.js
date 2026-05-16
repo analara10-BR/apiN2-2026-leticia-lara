5da71a1c7e5d4b5df1610979c25d94c3a9455979
leticiaribeiro09 <leticia.ribeiro09@aluno.ifce.edu.br>
2026-05-16 10:45:18 -0300

    Update products.routes.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -6,9 +6,12 @@ const router = Router();
 const controller = new ProductsController();
 
 router.get("/products", (req, res, next) => {
-
     controller.getProducts(req, res, next);
-
+    
+router.get("/products/:id", (req, res, next) =>
+    controller.getProductById(req, res, next)
+);
+    
 });
 
 export { router };
