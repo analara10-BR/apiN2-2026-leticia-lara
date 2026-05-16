117ad9a0816b9cd8eeafd95ff7f2b5295a4234f2
ursinho-dev <urciano.ferreira10@aluno.ifce.edu.br>
2026-05-16 10:30:26 -0300

    update scripts

---
+++ public/scripts/products.js
@@ -0,0 +1,29 @@
+async function carregarProdutos() {
+
+    const response = await fetch("/api/products");
+
+    const dados = await response.json();
+
+    const container = document.getElementById("container-produtos");
+
+    console.log(dados);
+
+    container.innerHTML = dados.productsList
+        .map(
+            (p) => `
+            <div class="card">
+
+                <h3>${p.nome}</h3>
+
+                <p>
+                    ${p.disponivel ? "Disponível" : "Indisponível"}
+                </p>
+
+            </div>
+        `
+        )
+        .join("");
+
+}
+
+window.onload = carregarProdutos;
