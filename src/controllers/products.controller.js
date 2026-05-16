95cf96046ae3c201828a652c50ddd0fe8f463d0e
leticiaribeiro09 <leticia.ribeiro09@aluno.ifce.edu.br>
2026-05-16 10:49:31 -0300

    Update products.controller.js

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -46,4 +46,58 @@ export default class ProductsController {
 
     }
 
+    async getProductById(req, res, next) {
+
+        try {
+
+            const { id } = req.params;
+
+            const products = [
+
+                {
+                    id: 1,
+                    nome: "Mini saia",
+                    disponivel: true
+                },
+
+                {
+                    id: 2,
+                    nome: "Calça cargo",
+                    disponivel: true
+                },
+
+                {
+                    id: 3,
+                    nome: "Vestido",
+                    disponivel: false
+                },
+
+                {
+                    id: 4,
+                    nome: "Cropped",
+                    disponivel: false
+                }
+
+            ];
+
+            const product = products.find(
+                p => p.id == id
+            );
+
+            if (!product) {
+                return res.status(404).json({
+                    message: "Produto não encontrado"
+                });
+            }
+
+            res.json(product);
+
+        } catch (error) {
+
+            next(error);
+
+        }
+
+    }
+
 }
